c728f819ed323448a7ad0060485ede6171d78c8e
Tony Cao <tonycao0918@qq.com>
2022-10-22 12:07:07 +0800

    format code and add pre-commit check

--- libs/stitch/func.py
+++ libs/stitch/func.py
@@ -11,126 +11,152 @@ from pystackreg import StackReg
 import time
 from .flat_estimate import FlatEstimate
 
+
 class StitchTool:
-    def __init__(self,flat=None,bg=None) -> None:
+    def __init__(self, flat=None, bg=None) -> None:
         self.flat = flat
         self.bg = bg
         self.sr = StackReg(StackReg.RIGID_BODY)
         self.flat_estimate = FlatEstimate()
 
-    def set_flat(self,flat_info):
+    def set_flat(self, flat_info):
         self.flat_info = flat_info
 
-
-
-    def correct(self,src,bias):
-        if 'estimate' in self.flat_info:
+    def correct(self, src, bias):
+        if "estimate" in self.flat_info:
             flat, bg = self.flat_estimate(src)
-        elif self.flat_info['flat'] is None:
+        elif self.flat_info["flat"] is None:
             return src
         else:
-            flat = io.imread(self.flat_info['flat']) 
-            bg = io.imread(self.flat_info['bg'])
-        
-        if bias!=0:
-            bg = flat.min() -10 - bias   
-        print('---- bg max/min:',bg.max(),bg.min())
-        print('---- flat max/min:',flat.max(),flat.min())
+            flat = io.imread(self.flat_info["flat"])
+            bg = io.imread(self.flat_info["bg"])
+
+        if bias != 0:
+            bg = flat.min() - 10 - bias
+        print("---- bg max/min:", bg.max(), bg.min())
+        print("---- flat max/min:", flat.max(), flat.min())
 
         flat = flat.astype(np.float32)
         src = src.astype(np.float32)
 
         # bg = 7000
         for i in range(src.shape[0]):
-            raw = src[i,:,:]
-            raw = 50*((raw-bg)/(flat-bg))   # why 50?
-            src[i,:,:] = raw
-           
+            raw = src[i, :, :]
+            raw = 50 * ((raw - bg) / (flat - bg))  # why 50?
+            src[i, :, :] = raw
+
         # 这样映射合适吗？
-        src = src-src.min()
-        src = src/(src.max()/65535.0)*0.8
+        src = src - src.min()
+        src = src / (src.max() / 65535.0) * 0.8
         src = src.astype(np.uint16)
         return src
-    
-    def average_img(self,images,average_num,is_registration=False):
-        img_avg = np.zeros((images.shape[0]//average_num,images.shape[1],images.shape[2]),dtype=np.uint16)
-        for i in range(images.shape[0]//average_num):
-            img_stack = images[i*average_num:(i+1)*average_num]
+
+    def average_img(self, images, average_num, is_registration=False):
+        img_avg = np.zeros(
+            (images.shape[0] // average_num, images.shape[1], images.shape[2]),
+            dtype=np.uint16,
+        )
+        for i in range(images.shape[0] // average_num):
+            img_stack = images[i * average_num : (i + 1) * average_num]
             if is_registration:
-                out_mean_af = self.sr.register_stack(img_stack, axis=0, reference = 'previous' ) #输出的是形变矩阵
+                out_mean_af = self.sr.register_stack(
+                    img_stack, axis=0, reference="previous"
+                )  # 输出的是形变矩阵
                 img_stack = self.sr.transform_stack(img_stack)
-            img_avg[i] = np.mean(img_stack,axis=0)
+            img_avg[i] = np.mean(img_stack, axis=0)
         return img_avg
 
-    def stitch(self,img_path,save_path,grid_shape, average_num=1, bias=-1500, overlay=0.1, fill=True, is_registration=False):
+    def stitch(
+        self,
+        img_path,
+        save_path,
+        grid_shape,
+        average_num=1,
+        bias=-1500,
+        overlay=0.1,
+        fill=True,
+        is_registration=False,
+    ):
         if is_registration:
-            print('开启配准,耗时很长请等待...')
-        [row_num,col_num] = grid_shape
+            print("开启配准,耗时很长请等待...")
+        [row_num, col_num] = grid_shape
         images = io.imread(img_path).astype(np.float32)
-        if images.shape[0] != grid_shape[0]*grid_shape[1]*average_num:
-            new_img = np.zeros((grid_shape[0]*grid_shape[1]*average_num, images.shape[1], images.shape[2]), dtype=np.uint16)
+        if images.shape[0] != grid_shape[0] * grid_shape[1] * average_num:
+            new_img = np.zeros(
+                (
+                    grid_shape[0] * grid_shape[1] * average_num,
+                    images.shape[1],
+                    images.shape[2],
+                ),
+                dtype=np.uint16,
+            )
             if fill:
-                print('图片缺帧,已在尾部补帧')
-                new_img[:images.shape[0]] = images
+                print("图片缺帧,已在尾部补帧")
+                new_img[: images.shape[0]] = images
             else:
-                print('图片缺帧,已在头部补帧')
-                new_img[-images.shape[0]:] = images
+                print("图片缺帧,已在头部补帧")
+                new_img[-images.shape[0] :] = images
 
             images = new_img
-        images = self.average_img(images,average_num,is_registration)
+        images = self.average_img(images, average_num, is_registration)
         images = self.correct(images, bias)
 
-
-    
         images = images.astype(np.uint16)
-        rows,cols = [],[]
+        rows, cols = [], []
         for row in range(row_num):
-            rows += [row]*col_num
-            if row%2==0:
+            rows += [row] * col_num
+            if row % 2 == 0:
                 cols += list(range(col_num))
             else:
-                cols += list(range(col_num-1,-1,-1))
-    
+                cols += list(range(col_num - 1, -1, -1))
 
         zzz = images
-        output = np.zeros((zzz.shape[1]*row_num,zzz.shape[2]*col_num),dtype=images.dtype)
+        output = np.zeros(
+            (zzz.shape[1] * row_num, zzz.shape[2] * col_num), dtype=images.dtype
+        )
 
-        h,w = images.shape[1], images.shape[2]
-        hide = int(overlay*images.shape[1])
+        h, w = images.shape[1], images.shape[2]
+        hide = int(overlay * images.shape[1])
         # print('重叠量：',overlay)
         step = zzz.shape[1] - hide
 
-        overlay_map = np.zeros_like(output,dtype=np.uint8)
+        overlay_map = np.zeros_like(output, dtype=np.uint8)
 
         for i in range(zzz.shape[0]):
-            row,col = rows[i], cols[i]
-            x0,x1 = step*col, step*col+w
-            y0,y1 = step*row, step*row+h
+            row, col = rows[i], cols[i]
+            x0, x1 = step * col, step * col + w
+            y0, y1 = step * row, step * row + h
 
-            image = zzz[i,:,:]
-            patch = output[y0:y1,x0:x1]
-            overlay_patch = overlay_map[y0:y1,x0:x1]
+            image = zzz[i, :, :]
+            patch = output[y0:y1, x0:x1]
+            overlay_patch = overlay_map[y0:y1, x0:x1]
 
             if overlay_patch.sum() != 0:
-                weight_overlay = ndimage.morphology.distance_transform_edt(overlay_patch).astype(np.float64)
+                weight_overlay = ndimage.morphology.distance_transform_edt(
+                    overlay_patch
+                ).astype(np.float64)
                 max_value = np.partition(weight_overlay.flatten(), -2)[-2]
                 weight_overlay /= max_value
-                weight_overlay = np.clip(weight_overlay,0,1)
+                weight_overlay = np.clip(weight_overlay, 0, 1)
             else:
-                weight_overlay = np.zeros_like(patch,dtype=np.float64)
-            
-            weight_no_overlay =np.ones_like(weight_overlay,dtype=np.float64)-weight_overlay
-            patch = patch*weight_overlay + image*weight_no_overlay
-            
-        
-            overlay_map[y0:y1,x0:x1] = 1
-            output[y0:y1,x0:x1] = patch     # patch是float型而output是整型。请确保灰度级够大（不够大就乘个系数），否则这零点几的精度损失会带来明显的视觉差异
-        print('>>>>  finish',save_path)
-        io.imsave(save_path,output)
+                weight_overlay = np.zeros_like(patch, dtype=np.float64)
+
+            weight_no_overlay = (
+                np.ones_like(weight_overlay, dtype=np.float64) - weight_overlay
+            )
+            patch = patch * weight_overlay + image * weight_no_overlay
+
+            overlay_map[y0:y1, x0:x1] = 1
+            output[
+                y0:y1, x0:x1
+            ] = patch  # patch是float型而output是整型。请确保灰度级够大（不够大就乘个系数），否则这零点几的精度损失会带来明显的视觉差异
+        print(">>>>  finish", save_path)
+        io.imsave(save_path, output)
         return output
 
-if __name__=='__main__':
-    aaa = np.zeros((10,512,512))
+
+if __name__ == "__main__":
+    aaa = np.zeros((10, 512, 512))
     aaa = reg_img_stack(aaa)
     print(aaa.shape)
     # stitch_tool = StitchTool()
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time,os,sys,cv2,qdarkstyle
+import time, os, sys, cv2, qdarkstyle
 
 from libs.stitch import StitchTool
 from libs.show.show_tool import SingleShowWindow
-sys.path.append('libs/Stitch')
+
+sys.path.append("libs/Stitch")
 import numpy as np
 from PIL import Image
-#import QMutex
+
+# import QMutex
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -17,37 +19,30 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from libs import *
 
 
-Denoise_func = {
-    'gaussian': gaussianBlur,
-    'mean': meanBlur,
-    'median': medianBlur
-}
-
+Denoise_func = {"gaussian": gaussianBlur, "mean": meanBlur, "median": medianBlur}
 
 
 class GUI(QMainWindow):
     def __init__(self):
         super(GUI, self).__init__()
-        uic.loadUi("./assets/main.ui", self)   # 以文件形式加载ui界面
-        
+        uic.loadUi("./assets/main.ui", self)  # 以文件形式加载ui界面
+
         self.img_list = []
-        #self.openImg.triggered.connect(self.open_image)
+        # self.openImg.triggered.connect(self.open_image)
         self.button_openimg_stitch.clicked.connect(self.open_image_stitch)
 
         self.button_stitch.clicked.connect(self.stitch)
         self.button_stitch_batch.clicked.connect(self.stitch_batch)
 
-        self.denoise_func = 'gaussian'
+        self.denoise_func = "gaussian"
         self.Denoise_size = {
-            'gaussian': self.slider_gaussian,
-            'mean': self.slider_mean,
-            'median': self.slider_median
+            "gaussian": self.slider_gaussian,
+            "mean": self.slider_mean,
+            "median": self.slider_median,
         }
         self.overlay = int(self.line_overlay.text())
 
-
         self.button_merge.clicked.connect(self.open_merge_gui)
-
 
         self.button_gaussian.clicked.connect(self.set_gaussion)
         self.button_mean.clicked.connect(self.set_mean)
@@ -57,56 +52,68 @@ class GUI(QMainWindow):
         self.slider_mean.valueChanged.connect(self.frame_change)
         self.slider_median.valueChanged.connect(self.frame_change)
 
-        self.stitch_tool = StitchTool(flat='assets/flat/400/flat.tif',bg='assets/flat/400/bg.tif')
+        self.stitch_tool = StitchTool(
+            flat="assets/flat/400/flat.tif", bg="assets/flat/400/bg.tif"
+        )
         self.load_flat()
+
     def open_merge_gui(self):
-        openfile = QFileDialog.getExistingDirectory(None, '选择文件夹', '.')
+        openfile = QFileDialog.getExistingDirectory(None, "选择文件夹", ".")
         if openfile:
-            self.merge_gui = MergeTool(project_path=openfile, ui_path='assets/channel_merge.ui')
+            self.merge_gui = MergeTool(
+                project_path=openfile, ui_path="assets/channel_merge.ui"
+            )
             self.merge_gui.show()
             app.installEventFilter(self.merge_gui)
-        
+
     def open_image(self):
-        openfile = QFileDialog.getOpenFileNames(self, '选择文件', '', 'image files(*.tif)')[0]
+        openfile = QFileDialog.getOpenFileNames(self, "选择文件", "", "image files(*.tif)")[
+            0
+        ]
         if openfile:
             ret, images = cv2.imreadmulti(openfile[0], [], cv2.IMREAD_ANYCOLOR)
             if ret:
                 self.img_list = images
                 self.plot(images[0])
-                self.frame_slider.setMaximum(len(images)-1)
+                self.frame_slider.setMaximum(len(images) - 1)
                 self.frame_slider.setValue(0)
 
     def open_image_stitch(self):
-        openfile = QFileDialog.getExistingDirectory(None, '选择文件夹', '.')
+        openfile = QFileDialog.getExistingDirectory(None, "选择文件夹", ".")
         if openfile:
             self.line_imgpath_stitch.setText(openfile)
- 
-    def stitch(self,no_show=False):
+
+    def stitch(self, no_show=False):
         # try:
         base_dir = self.line_imgpath_stitch.text()
         print(base_dir)
-        src_path = os.path.join(base_dir, r'CellVideo/CellVideo 0.tif')
-        save_path = src_path.replace('.tif','_stitch.tif')
+        src_path = os.path.join(base_dir, r"CellVideo/CellVideo 0.tif")
+        save_path = src_path.replace(".tif", "_stitch.tif")
         if not os.path.exists(src_path):
-            print('--- 【ERROR】 没有找到tif图像！')
+            print("--- 【ERROR】 没有找到tif图像！")
             return
-        img_avg_num = int(self.line_avg_num.text()) 
+        img_avg_num = int(self.line_avg_num.text())
         grid_shape = [int(self.line_grid1.text()), int(self.line_grid2.text())]
         bias = float(self.line_correct_bias.text())
 
         # 读取光场图像
         flat_info = self.flat_folder[self.combo_flat_name.currentText()]
         self.stitch_tool.set_flat(flat_info)
-        print('使用光场:',flat_info)
+        print("使用光场:", flat_info)
         # try:
-        img_out = self.stitch_tool.stitch(src_path,save_path,
+        img_out = self.stitch_tool.stitch(
+            src_path,
+            save_path,
             grid_shape,
             average_num=img_avg_num,
-            overlay=float(self.line_overlay.text())/100.0,
+            overlay=float(self.line_overlay.text()) / 100.0,
             bias=bias,
-            is_registration=self.reg_box.isChecked())
+            is_registration=self.reg_box.isChecked(),
+        )
         if not no_show:
-            self.single_show_window = SingleShowWindow(img=img_out, ui_path='./assets/single_show.ui')
+            self.single_show_window = SingleShowWindow(
+                img=img_out, ui_path="./assets/single_show.ui"
+            )
             self.single_show_window.show()
             app.installEventFilter(self.single_show_window)
         # except:
@@ -116,7 +123,7 @@ class GUI(QMainWindow):
 
     def load_txt(self, txt):
         info = {}
-        with open(txt, 'r') as f:
+        with open(txt, "r") as f:
             lines = f.readlines()
             for line in lines:
                 [name, value] = line.split()
@@ -126,33 +133,42 @@ class GUI(QMainWindow):
     def stitch_batch(self):
         base_dir = self.line_imgpath_stitch.text()
         target_dirs = os.listdir(base_dir)
-        print('【BATCH】',base_dir)
+        print("【BATCH】", base_dir)
         for target_dir in target_dirs:
             target_dir = os.path.join(base_dir, target_dir)
             if not os.path.isdir(target_dir):
                 continue
-            src_path = os.path.join(target_dir, r'CellVideo/CellVideo 0.tif')
-            save_path = src_path.replace('.tif','_stitch.tif')
+            src_path = os.path.join(target_dir, r"CellVideo/CellVideo 0.tif")
+            save_path = src_path.replace(".tif", "_stitch.tif")
             if not os.path.exists(src_path):
-                print('--- 【ERROR】:',src_path,' NOT FOUND!')
+                print("--- 【ERROR】:", src_path, " NOT FOUND!")
                 continue
-            print('>>>> Processing ', src_path)
-            info = self.load_txt(os.path.join(target_dir,'Information.txt'))
-            img_avg_num = int(info['Frames_per_slice']) 
-            grid_shape = [int(info['Vertical_Pages']), int(info['Horizontal_Pages'])]
+            print(">>>> Processing ", src_path)
+            info = self.load_txt(os.path.join(target_dir, "Information.txt"))
+            img_avg_num = int(info["Frames_per_slice"])
+            grid_shape = [int(info["Vertical_Pages"]), int(info["Horizontal_Pages"])]
             bias = float(self.line_correct_bias.text())
 
             # 读取光场图像
             flat_info = self.flat_folder[self.combo_flat_name.currentText()]
             self.stitch_tool.set_flat(flat_info)
             try:
-                self.stitch_tool.stitch(src_path,save_path,grid_shape,average_num=img_avg_num,overlay=float(self.line_overlay.text())/100.0,bias=bias,is_registration=self.reg_box.isChecked())
+                self.stitch_tool.stitch(
+                    src_path,
+                    save_path,
+                    grid_shape,
+                    average_num=img_avg_num,
+                    overlay=float(self.line_overlay.text()) / 100.0,
+                    bias=bias,
+                    is_registration=self.reg_box.isChecked(),
+                )
             except:
-                msg_box = QMessageBox(QMessageBox.Warning, 'Warning', '发生未知错误，请检查参数是否正确！')
+                msg_box = QMessageBox(
+                    QMessageBox.Warning, "Warning", "发生未知错误，请检查参数是否正确！"
+                )
                 msg_box.exec_()
                 return
-        QMessageBox.information(self, 'Result', src_path+'\n拼接批处理完毕！') 
-
+        QMessageBox.information(self, "Result", src_path + "\n拼接批处理完毕！")
 
     def frame_change(self):
         if self.img_list:
@@ -160,36 +176,41 @@ class GUI(QMainWindow):
             self.plot(self.img_list[id])
 
     def load_flat(self):
-        root = 'assets/flat'
+        root = "assets/flat"
         folder_names = os.listdir(root)
-        self.flat_folder = {'None':{'flat':None,'bg':None}, 'Estimate':{'estimate'}}
+        self.flat_folder = {
+            "None": {"flat": None, "bg": None},
+            "Estimate": {"estimate"},
+        }
         for folder_name in folder_names:
-            folder_path = os.path.join(root,folder_name)
-            flat_path = os.path.join(folder_path,'flat.tif')
-            bg_path = os.path.join(folder_path,'bg.tif')
+            folder_path = os.path.join(root, folder_name)
+            flat_path = os.path.join(folder_path, "flat.tif")
+            bg_path = os.path.join(folder_path, "bg.tif")
             if os.path.isfile(flat_path) and os.path.exists(bg_path):
-                self.flat_folder[folder_name] = {'flat':flat_path,'bg':bg_path}
+                self.flat_folder[folder_name] = {"flat": flat_path, "bg": bg_path}
                 self.combo_flat_name.addItem(folder_name)
-    
+
     def set_gaussion(self):
-        self.denoise_func = 'gaussian'
+        self.denoise_func = "gaussian"
         self.frame_change()
 
     def set_mean(self):
-        self.denoise_func = 'mean'
+        self.denoise_func = "mean"
         self.frame_change()
 
     def set_median(self):
-        self.denoise_func = 'median'
+        self.denoise_func = "median"
         self.frame_change()
 
-    def plot(self,img):
+    def plot(self, img):
         """
         将图像显示在QLabel上
         """
-   
-        img = cv2.resize(img.astype(np.uint8),(512,512))
-        img_denoise = Denoise_func[self.denoise_func](img, self.Denoise_size[self.denoise_func].value())
+
+        img = cv2.resize(img.astype(np.uint8), (512, 512))
+        img_denoise = Denoise_func[self.denoise_func](
+            img, self.Denoise_size[self.denoise_func].value()
+        )
 
         if self.tabWidget.currentIndex() == 0:
             img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
@@ -197,11 +218,16 @@ class GUI(QMainWindow):
             self.curve_origin.setPixmap(QPixmap.fromImage(img))
 
             img_denoise = cv2.cvtColor(img_denoise, cv2.COLOR_RGB2BGR)
-            img_denoise = QImage(img_denoise.data, img_denoise.shape[1], img_denoise.shape[0], QImage.Format_RGB888)
+            img_denoise = QImage(
+                img_denoise.data,
+                img_denoise.shape[1],
+                img_denoise.shape[0],
+                QImage.Format_RGB888,
+            )
             self.curve_denoise.setPixmap(QPixmap.fromImage(img_denoise))
 
         elif self.tabWidget.currentIndex() == 1:
-            if len(img.shape)==2:
+            if len(img.shape) == 2:
                 img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
             img = QImage(img.data, 860, 640, QImage.Format_RGB888)
             self.label_stitch.setPixmap(QPixmap.fromImage(img))
@@ -210,18 +236,20 @@ class GUI(QMainWindow):
     def detect_img(self):
         if self.cap_thread.is_running():
             self.cap_thread.stop()
-        file = QFileDialog.getOpenFileName(self, '选择图片', '', 'images(*.png; *.jpg);;*')[0]
+        file = QFileDialog.getOpenFileName(self, "选择图片", "", "images(*.png; *.jpg);;*")[
+            0
+        ]
         if not file:
             return
         img = cv2.imread(file)
-        #检测图片
+        # 检测图片
         r_image = self.model.detect_image(Image.fromarray(np.uint8(img)))
         img = np.array(r_image)
         # 显示图片
         self.plot(img)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     stylesheet = qdarkstyle.load_stylesheet_pyqt5()
     app.setFont(QFont("微软雅黑", 9))
