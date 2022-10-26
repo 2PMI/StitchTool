67a3b226509a42bc8a843712c0da59c23f7fdc87
Tony Cao <tonycao0918@qq.com>
2022-10-26 19:33:34 +0800

    add more flat average

--- libs/stitch/naive_estimate.py
+++ libs/stitch/naive_estimate.py
@@ -24,7 +24,6 @@ class NaiveEstimate:
             bgs.append([img, self.LCoV_evaluate(img)])
 
         bgs.sort(key=lambda x: x[1])
-        bg = bgs[0][0]
 
         # 亮度阈值为：不超过当前最暗的合成图的最亮的像素值亮度的0.05%
         intensity_threshold = np.max(bgs[0][0]) * (1.0 + 0.005)
@@ -33,28 +32,27 @@ class NaiveEstimate:
         for img, _ in bgs[:6]:
             if img.max() < intensity_threshold:
                 valid_bgs.append(img)
-
         bg = sum(valid_bgs) / len(valid_bgs)
 
         # print("intentisy threshold:", intensity_threshold)
-        # print("valid bg num:", len(valid_bgs))
+        print("valid background numbers:", len(valid_bgs))
 
-        for img in reconstructed_imgs[:10]:  # 只取前10张图，找其中最平滑的
+        for img in reconstructed_imgs[:85]:  # 取前85张图，找其中最平滑的
             img -= bg
             distortions.append([img, self.LCoV_evaluate(img)])
 
         # 平滑度阈值不超过最低的 0.5%
         distortions.sort(key=lambda x: x[1])
-        flat = distortions[0][0]
         LCoV_threshold = distortions[0][1] * (1.0 + 0.005)
 
         valid_flat = []
-        for img, LCoV_score in distortions[:6]:
+        for img, LCoV_score in distortions[:10]:
             if LCoV_score < LCoV_threshold:
                 valid_flat.append(img)
 
         flat = sum(valid_flat) / len(valid_flat)
 
+        print("valid flat numbers:", len(valid_flat))
         # io.imsave("flat.tif", distortions[0][0].astype(np.uint16))
         # TODO:数学推导模拟
         flat = gaussian_filter(flat, sigma=10, mode="nearest")
